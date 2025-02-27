1c3817351ca68701c28abc29ddf4e3227e9bdef8
Zhuldyzay-zhak <zhaksymbetova97@mail.ru>
2025-02-27 16:21:34 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InputFile
 import sqlite3
 

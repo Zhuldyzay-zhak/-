6459bc786f4fd87bc282d3bb236c2ee951b5cb6e
Zhuldyzay-zhak <zhaksymbetova97@mail.ru>
2025-02-28 17:40:00 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,26 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from fastapi import FastAPI
 import asyncio
+import logging
+import os
 
-TOKEN = "7781455167:AAGvyYjViBDEEXxPRofSYgJ7nAnmlAINLbg"
+TOKEN = "ТВОЙ_НОВЫЙ_ТОКЕН"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+# Настраиваем FastAPI
+app = FastAPI()
+
+@app.post("/")
+async def webhook(update: dict):
+    telegram_update = Update(**update)
+    await dp.process_update(telegram_update)
+
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    await bot.set_webhook("7781455167:AAGvyYjViBDEEXxPRofSYgJ7nAnmlAINLbg")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":

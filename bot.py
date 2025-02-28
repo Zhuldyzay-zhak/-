7307b3ef49cc788479c99faa15a810514af1341c
Zhuldyzay-zhak <zhaksymbetova97@mail.ru>
2025-02-28 18:37:51 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 async def main():
-    await bot.delete_webhook()  # убедитесь, что этот код имеет правильный отступ
-    await dp.start_polling()
+    await bot.delete_webhook()  # Удаляем Webhook
+    await dp.start_polling()  # Запускаем бота
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())

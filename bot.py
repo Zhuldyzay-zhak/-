894a010e8b3085839bab8f269ad087325bcce0f5
Zhuldyzay-zhak <zhaksymbetova97@mail.ru>
2025-02-28 18:17:19 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,14 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Update
-from fastapi import FastAPI
 import asyncio
-import logging
-import os
+from aiogram import Bot, Dispatcher
 
 TOKEN = "7781455167:AAGvyYjViBDEEXxPRofSYgJ7nAnmlAINLbg"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Настраиваем FastAPI
-app = FastAPI()
-
-@app.post("/")
-async def webhook(update: dict):
-    telegram_update = Update(**update)
-    await dp.process_update(telegram_update)
-
 async def main():
-    logging.basicConfig(level=logging.INFO)
-    await bot.delete_webhook()
-await bot.start_polling()
-
-    await dp.start_polling(bot)
+    await bot.delete_webhook()  # убедитесь, что этот код имеет правильный отступ
+    await dp.start_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
